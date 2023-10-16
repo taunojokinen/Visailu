16be9ca657e6661e8429357269892a893d01e65e
taunojokinen <128710496+taunojokinen@users.noreply.github.com>
2023-10-16 09:20:23 +0300

    kysymykset lisätty

---
+++ demo/src/main/java/com/example/demo/VisailuRestControllers.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VisailuRestControllers {
+    @GetMapping("/")
+    public String Homepage(){
+        return ("wellcome to Visailu!")
+    }
+}
