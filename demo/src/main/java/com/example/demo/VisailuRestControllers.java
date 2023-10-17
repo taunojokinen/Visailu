f17aca574cfa3665eba202a50bd0f4cc401632dd
taunojokinen <128710496+taunojokinen@users.noreply.github.com>
2023-10-17 13:44:16 +0300

    publishing

--- demo/src/main/java/com/example/demo/VisailuRestControllers.java
+++ demo/src/main/java/com/example/demo/VisailuRestControllers.java
@@ -1,10 +1,7 @@
 package com.example.demo;
-
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
